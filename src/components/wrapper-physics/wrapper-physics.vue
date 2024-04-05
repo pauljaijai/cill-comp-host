ab22de72c6a5c0ae4699692eefadd9f6957cd693
Codfisher <pony2140@livemail.tw>
2024-04-05 15:36:14 +0800

    feat: 新增 wrapper-physics

---
+++ src/components/wrapper-physics/wrapper-physics.vue
@@ -0,0 +1,24 @@
+<!-- <template></template> -->
+
+<script setup lang="ts">
+import { VNode, ref } from 'vue';
+
+interface Props {
+  modelValue?: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: '',
+});
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: Props['modelValue']): void;
+}>();
+
+defineSlots<{
+  default?: () => VNode;
+  item?: (props: { id: number }) => VNode;
+}>();
+</script>
+
+<style scoped lang="sass">
+</style>
