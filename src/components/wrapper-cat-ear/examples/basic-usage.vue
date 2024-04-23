558c9bc971c70027f7baa5ac1f6ec7911206ca4f
Codfisher <pony2140@livemail.tw>
2024-04-24 00:10:05 +0800

    feat: 調整範例內容

--- src/components/wrapper-cat-ear/examples/basic-usage.vue
+++ src/components/wrapper-cat-ear/examples/basic-usage.vue
@@ -2,21 +2,21 @@
   <div class="w-full py-16 border border-gray-300 flex flex-col gap-16 justify-center items-center">
     <wrapper-cat-ear>
       <div
-        class=" rounded p-2 bg-[#e4e4e4]"
+        class="border rounded p-2 bg-white"
         v-text="`・ω・`"
       />
     </wrapper-cat-ear>
 
     <wrapper-cat-ear>
       <div
-        class=" rounded px-6 py-10 bg-[#e4e4e4]"
+        class="border rounded px-6 py-10 bg-white"
         v-text="`ʘ ᴥ ʘ`"
       />
     </wrapper-cat-ear>
 
     <wrapper-cat-ear>
       <div
-        class=" rounded px-10 py-2 bg-[#e4e4e4]"
+        class="border rounded px-10 py-2 bg-white"
         v-text="`◕ ω ◕`"
       />
     </wrapper-cat-ear>
--- src/components/wrapper-cat-ear/examples/select-emotion.vue
+++ src/components/wrapper-cat-ear/examples/select-emotion.vue
@@ -1,38 +1,26 @@
 <template>
   <div class="w-full py-10 border border-gray-300 flex flex-col gap-16 justify-center items-center">
-    <div class=" border border-gray-300 rounded">
-      <select
-        v-model="emotion"
-        class="  p-2"
-      >
-        <option
-          v-for="option in emotionOptions"
-          :key="option"
-          :value="option"
+    <wrapper-cat-ear :emotion="emotion">
+      <div class=" border border-gray-300 rounded">
+        <select
+          v-model="emotion"
+          class="  p-2"
         >
-          {{ option }}
-        </option>
-      </select>
-    </div>
-
-    <wrapper-cat-ear :emotion="emotion">
-      <div
-        class="border rounded p-2 bg-white"
-        v-text="`・ω・`"
-      />
+          <option
+            v-for="option in emotionOptions"
+            :key="option"
+            :value="option"
+          >
+            {{ option }}
+          </option>
+        </select>
+      </div>
     </wrapper-cat-ear>
 
     <wrapper-cat-ear :emotion="emotion">
       <div
-        class="border rounded px-6 py-10 bg-white"
-        v-text="`ʘ ᴥ ʘ`"
-      />
-    </wrapper-cat-ear>
-
-    <wrapper-cat-ear :emotion="emotion">
-      <div
-        class="border rounded px-14 py-10 bg-white"
-        v-text="`◕ ω ◕`"
+        class="border rounded px-6 py-4 bg-white"
+        v-text="`◔ ω ◔`"
       />
     </wrapper-cat-ear>
   </div>
