944c94d7903fb90fa876f0142a6ce7b2703ae896
Codfisher <pony2140@livemail.tw>
2024-04-24 22:44:53 +0800

    feat: 完成 interactive-effect

--- src/components/wrapper-cat-ear/examples/interactive-effect.vue
+++ src/components/wrapper-cat-ear/examples/interactive-effect.vue
@@ -2,21 +2,20 @@
   <div class="w-full py-20 border border-gray-300 flex flex-col gap-16 justify-center items-center">
     <wrapper-cat-ear
       :emotion="emotion"
-      main-color="#4fb3ff"
-      inner-color="#ffc2b8"
-      class="cat"
+      main-color="#DDD"
+      class="cat border rounded "
     >
       <div
         ref="catRef"
-        class=" rounded px-[4rem] py-[3rem] bg-[#4fb3ff] flex justify-center items-center "
+        class=" px-16 py-8 bg-white flex justify-center items-center"
       >
         <transition
           name="cat"
           mode="out-in"
         >
           <div
-            class=" absolute text-nowrap select-none"
             :key="face"
+            class=" absolute text-2xl font-bold text-nowrap select-none"
             v-text="face"
           />
         </transition>
@@ -46,16 +45,22 @@ const {
   elementWidth, elementHeight,
   isOutside,
 } = useMouseInElement(catRef);
-const { pressed } = useMousePressed()
+const { pressed } = useMousePressed();
 
 /** 滑鼠與貓的距離 */
 const distance = computed(() => getVectorLength({
   x: elementX.value - elementWidth.value / 2,
   y: elementY.value - elementHeight.value / 2,
 }));
+/** 煩躁的距離 */
 const displeasedDistance = computed(
   () => Math.max(elementWidth.value, elementHeight.value)
 );
+/** 變成貓的距離 */
+const catDistance = computed(
+  () => displeasedDistance.value * 2
+);
+
 
 const emotion = computed<`${EmotionName}`>(() => {
   if (!isOutside.value) {
@@ -68,19 +73,28 @@ const emotion = computed<`${EmotionName}`>(() => {
 
   return 'relaxed';
 });
-const face = computed(() => faceMap[emotion.value]);
+const face = computed(() => {
+  if (distance.value > catDistance.value) {
+    return '送出';
+  }
+
+  return faceMap[emotion.value]
+});
 </script>
 
 <style lang="sass">
 .cat
   transition-duration: 0.4s
+  box-shadow: 4px 4px 0px 0px #AAA
   &:hover
     transition-duration: 0.3s
     scale: 1.02
+    box-shadow: 6px 6px 0px 0px #CCC
   &:active
     transition-duration: 0.1s
-    opacity: 0.5
     scale: 0.95
+    animation: shake 0.3s infinite ease-in-out
+    box-shadow: 0px 0px 0px 0px #888
 
 .cat-enter-active, .cat-leave-active
   transition-duration: 0.2s
@@ -89,4 +103,10 @@ const face = computed(() => faceMap[emotion.value]);
   opacity: 0 !important
 .cat-leave-to
   transform: translateY(-10px)
+
+@keyframes shake
+  35%
+    rotate: -3deg
+  70%
+    rotate: 3deg
 </style>
