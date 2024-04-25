a485f60406e7834c74f9ea59bcdd2b02cf0a5953
Codfisher <pony2140@livemail.tw>
2024-04-25 20:50:04 +0800

    feat: 調整 basic-usage

--- src/components/wrapper-cat-ear/examples/basic-usage.vue
+++ src/components/wrapper-cat-ear/examples/basic-usage.vue
@@ -1,24 +1,37 @@
 <template>
   <div class="w-full py-16 border border-gray-300 flex flex-col gap-16 justify-center items-center">
     <wrapper-cat-ear main-color="#AAA">
-      <div
-        class="border border-[#AAA] rounded p-2 bg-white"
-        v-text="`・ω・`"
-      />
+      <div class="border border-[#AAA] rounded  bg-white">
+        <button class="px-4 p-2">
+          按鈕
+        </button>
+      </div>
     </wrapper-cat-ear>
 
     <wrapper-cat-ear main-color="#AAA">
-      <div
-        class="border border-[#AAA] rounded px-6 py-10 bg-white"
-        v-text="`ʘ ᴥ ʘ`"
-      />
+      <div class="">
+        <img
+          class="rounded w-80"
+          src="/codfish.webp"
+        >
+      </div>
     </wrapper-cat-ear>
 
     <wrapper-cat-ear main-color="#AAA">
-      <div
-        class="border border-[#AAA] rounded px-10 py-2 bg-white"
-        v-text="`◕ ω ◕`"
-      />
+      <div class=" border border-[#AAA] rounded ">
+        <select class="px-10 py-2">
+          <option
+            disabled
+            selected
+            label="請選擇貓貓"
+          />
+          <option label="(>'-'<)ノ" />
+          <option label="~(=^‥^)" />
+          <option label="/ᐠ｡ꞈ｡ᐟ\" />
+          <option label="(^._.^)ﾉ" />
+          <option label="(^・ω・^ )" />
+        </select>
+      </div>
     </wrapper-cat-ear>
   </div>
 </template>
