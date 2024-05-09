6b8d6c42e131ca4b6e9bf15bbf192047fd6463ac
Codfisher <pony2140@livemail.tw>
2024-05-10 00:59:05 +0800

    feat: 完成 emit 功能

--- src/components/util-party-popper/examples/basic-usage.vue
+++ src/components/util-party-popper/examples/basic-usage.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="flex flex-col gap-4 w-full border border-gray-300">
-    <util-party-popper class=" flex h-[40vh]">
+    <util-party-popper
+      ref="popperRef"
+      class=" flex h-[40vh]"
+    >
       <div class=" w-full h-full flex justify-center items-center">
-        <div class="w-5 h-5 bg-white/70" />
+        <div
+          ref="btnRef"
+          class="border bg-white rounded px-4 py-2 select-none cursor-pointer"
+          @click="emit()"
+        >
+          按鈕
+        </div>
       </div>
     </util-party-popper>
   </div>
@@ -14,7 +23,23 @@ import { ref } from 'vue';
 import BaseCheckbox from '../../base-checkbox.vue';
 import BaseInput from '../../base-input.vue';
 import UtilPartyPopper from '../util-party-popper.vue';
+import { useElementBounding } from '@vueuse/core';
 
 const text = ref('');
 const disable = ref(true);
+
+const popperRef = ref<InstanceType<typeof UtilPartyPopper>>();
+const popperBounding = useElementBounding(popperRef);
+
+const btnRef = ref<HTMLDivElement>();
+const btnBounding = useElementBounding(btnRef);
+
+
+function emit() {
+  const param = {
+    x: btnBounding.left.value + btnBounding.width.value / 2 - popperBounding.left.value,
+    y: btnBounding.top.value + btnBounding.height.value / 2 - popperBounding.top.value,
+  }
+  popperRef.value?.emit(param);
+}
 </script>
--- src/components/util-party-popper/util-party-popper.vue
+++ src/components/util-party-popper/util-party-popper.vue
@@ -6,30 +6,73 @@
     />
 
     <slot />
+
+    <div class=" absolute top-0 left-0 p-4">
+      {{ engine?.getFps() }}
+    </div>
   </div>
 </template>
 
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref, shallowRef, toRefs } from 'vue';
 import { InitParam, useBabylonScene } from '../../composables/use-babylon-scene';
-import { ArcRotateCamera, Camera, MeshBuilder, Scalar, SolidParticleSystem, Vector3 } from '@babylonjs/core';
+import {
+  ArcRotateCamera, Camera,
+  Color3, Color4
+  , HemisphericLight, MeshBuilder, Scalar,
+  Scene, SolidParticle, SolidParticleSystem, Vector3
+} from '@babylonjs/core';
 import { useElementBounding } from '@vueuse/core';
+import { pipe } from 'remeda';
 
 // #region Props
 interface Props {
-  modelValue?: string;
+  /** 紙屑參數 */
+  confetti?: {
+    width: number;
+    height: number;
+  };
+
+  /** 每次發射數量
+   * 
+   * @default 10
+   */
+  quantityOfPerEmit?: number;
+  /** 最大粒子數量。
+   * 
+   * @default 100
+   */
+  maxQuantity?: number;
+
+  /** 最大角速度
+   * 
+   * @default 1.5
+   */
+  maxAngularVelocity?: number;
+  /** 重力
+   * 
+   * @default -0.01
+   */
+  gravity?: number;
+  /** 空氣阻力
+   * 
+   * @default 0.01
+   */
+  airResistance?: number;
 }
 // #endregion Props
 const props = withDefaults(defineProps<Props>(), {
-  modelValue: '',
+  confetti: () => ({
+    width: 10,
+    height: 15,
+  }),
+  quantityOfPerEmit: 10,
+  maxQuantity: 100,
+  maxAngularVelocity: Math.PI / 20,
+  gravity: -0.05,
+  airResistance: 0.01,
 });
-
-// #region Emits
-const emit = defineEmits<{
-  'update:modelValue': [value: Props['modelValue']];
-}>();
-// #endregion Emits
 
 // #region Slots
 defineSlots<{
@@ -37,13 +80,41 @@ defineSlots<{
 }>();
 // #endregion Slots
 
-const { canvasRef } = useBabylonScene({
+const {
+  confetti, maxAngularVelocity,
+  gravity,
+} = toRefs(props);
+
+const particleSystem = shallowRef<SolidParticleSystem>();
+const {
+  canvasRef,
+  engine,
+} = useBabylonScene({
+  createScene(engine) {
+    const scene = new Scene(engine);
+    // 背景透明
+    scene.clearColor = new Color4(0, 0, 0, 0);
+
+    /** 使用預設光源 */
+    scene.createDefaultLight();
+    const defaultLight = scene.lights.at(-1);
+
+    if (defaultLight instanceof HemisphericLight) {
+      defaultLight.intensity = 1;
+      defaultLight.direction = new Vector3(0.5, 1, 0);
+
+      defaultLight.diffuse = new Color3(1, 0.8, 0.8);
+      defaultLight.groundColor = new Color3(0.8, 0.8, 0.8);
+    }
+
+    return scene;
+  },
   createCamera(scene) {
     const camera = new ArcRotateCamera(
       'camera',
       Math.PI / 2,
       Math.PI / 2,
-      Math.max(width.value, height.value),
+      Math.max(elWidth.value, elHeight.value),
       new Vector3(0, 0, 0),
       scene
     );
@@ -52,54 +123,130 @@ const { canvasRef } = useBabylonScene({
     return camera;
   },
   async init(param) {
-    initParticles(param);
-
-    MeshBuilder.CreateBox('', {
-      width: 20,
-      height: 20,
-      depth: 20,
-    }, param.scene);
-
-    const box2 = MeshBuilder.CreateBox('', {
-      width: 20,
-      height: 20,
-      depth: 20,
-    }, param.scene);
-
-    console.log('width : ', width.value);
-    box2.position = new Vector3(width.value / 2, 0, 0);
+    particleSystem.value = initParticles(param);
   },
 });
-const { width, height } = useElementBounding(canvasRef);
+
+const { width: elWidth, height: elHeight } = useElementBounding(canvasRef);
+/** 畫布邊界 */
+const canvasBoundary = computed(() => {
+  const { width, height } = confetti.value;
+
+  return {
+    left: -elWidth.value / 2 - Math.max(width, height),
+    right: elWidth.value / 2 + Math.max(width, height),
+    bottom: -elHeight.value / 2 - Math.max(width, height),
+  }
+});
 
 function initParticles({ scene }: InitParam) {
-  const SPS = new SolidParticleSystem("SPS", scene);
-  const poly = MeshBuilder.CreatePolyhedron("p", { type: 2, size: 5 });
-  SPS.addShape(poly, 120);
-  poly.dispose();
+  const { width, height } = confetti.value;
 
-  const mesh = SPS.buildMesh();
+  const spSystem = new SolidParticleSystem('SPS', scene);
+  const box = MeshBuilder.CreateBox('box', { width, height, depth: 1 });
+  spSystem.addShape(box, props.maxQuantity);
+  box.dispose();
 
-  const [x, y] = [width.value / 2, height.value / 2];
+  const mesh = spSystem.buildMesh();
 
-  // initiate particles function
-  SPS.initParticles = () => {
-    for (let p = 0; p < SPS.nbParticles; p++) {
-      const particle = SPS.particles[p];
-      if (!particle) return;
+  spSystem.initParticles = () => {
+    particleSystem.value?.particles.forEach((particle) => {
+      particle.color = new Color4(
+        Scalar.RandomRange(0.5, 1),
+        Scalar.RandomRange(0.5, 1),
+        Scalar.RandomRange(0.5, 1),
+        1
+      );
 
-      particle.position.x = Scalar.RandomRange(-x, x);
-      particle.position.y = Scalar.RandomRange(-y, y);
-    }
+      initParticle(particle);
+
+      particle.position.y = canvasBoundary.value.bottom;
+    });
   };
 
-  //Update SPS mesh
-  SPS.initParticles();
-  SPS.setParticles();
+  spSystem.initParticles();
+  spSystem.setParticles();
+
+  spSystem.updateParticle = (particle) => {
+    if (particle.position.y < canvasBoundary.value.bottom
+      || particle.position.x < canvasBoundary.value.left
+      || particle.position.x > canvasBoundary.value.right
+    ) return particle;
+
+    particle.velocity.y += gravity.value;
+    particle.position.addInPlace(particle.velocity);
+
+    if (particle?.props?.rotationVelocity) {
+      particle.rotation.addInPlace(
+        particle.props.rotationVelocity
+      );
+    }
+
+    return particle;
+  }
+
+  /** 播放動畫 */
+  scene.onAfterRenderObservable.add(() => {
+    spSystem.setParticles();
+  })
+
+  return spSystem;
+}
+function initParticle(particle: SolidParticle) {
+  particle.velocity = new Vector3(
+    Scalar.RandomRange(-5, 5),
+    Scalar.RandomRange(-10, 10),
+    0
+  );
+
+  particle.rotation = new Vector3(
+    Scalar.RandomRange(0, 2 * Math.PI),
+    Scalar.RandomRange(0, 2 * Math.PI),
+    Scalar.RandomRange(0, 2 * Math.PI),
+  );
+
+  const minAngularVelocity = maxAngularVelocity.value * 0.6;
+  particle.props = {
+    rotationVelocity: new Vector3(
+      Scalar.RandomRange(minAngularVelocity, maxAngularVelocity.value),
+      Scalar.RandomRange(minAngularVelocity, maxAngularVelocity.value),
+      Scalar.RandomRange(minAngularVelocity, maxAngularVelocity.value),
+    )
+  };
+}
+
+interface EmitParam {
+  x: number;
+  y: number;
+}
+function emit(param: EmitParam) {
+  if (!particleSystem.value) return;
+
+  /** babylon 中心點為 0, 0，需要偏移 */
+  const { x, y } = pipe(param,
+    (data) => ({
+      x: data.x - elWidth.value / 2,
+      y: data.y - elHeight.value / 2,
+    }),
+  );
+
+  particleSystem.value?.particles.forEach((particle) => {
+    particle.color = new Color4(
+      Scalar.RandomRange(0.5, 1),
+      Scalar.RandomRange(0.5, 1),
+      Scalar.RandomRange(0.5, 1),
+      1
+    );
+    particle.position = new Vector3(x, y, 0);
+
+    initParticle(particle);
+  });
 }
 
 // #region Methods
-defineExpose({});
+defineExpose({
+  emit,
+});
 // #endregion Methods
 </script>
 
