ef35f0e889fe8decfc3fd08c7ec95514d438c19f
Codfisher <pony2140@livemail.tw>
2024-05-13 00:07:14 +0800

    feat: 嘗試載入圖片

--- src/components/carousel-colorful-particles/carousel-colorful-particles.vue
+++ src/components/carousel-colorful-particles/carousel-colorful-particles.vue
@@ -1,7 +1,14 @@
-<!-- <template></template> -->
+<template>
+  <div class=" overflow-hidden">
+    <img
+      src="/painting-codfish-rain.webp"
+      class=" object-cover w-full h-full"
+    >
+  </div>
+</template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 // #region Props
 interface Props {
@@ -23,6 +30,35 @@ defineSlots<{
   default?: () => unknown;
 }>();
 // #endregion Slots
+
+function loadImage(src: string) {
+  const image = new Image();
+  image.onload = function () {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    ctx.drawImage(image, 0, 0);
+
+    const pixelData = ctx.getImageData(0, 0, 1, 1).data;
+
+    const red = pixelData[0];
+    const green = pixelData[1];
+    const blue = pixelData[2];
+    const alpha = pixelData[3]; // Alpha 通道，表示透明度
+
+    // 輸出顏色值
+    console.log(`Color at (0, 0) - Red: ${red}, Green: ${green}, Blue: ${blue}, Alpha: ${alpha}`);
+  }
+  image.src = src;
+}
+
+onMounted(() => {
+  loadImage('/painting-codfish-rain.webp');
+});
 
 
 // #region Methods
