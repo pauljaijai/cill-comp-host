e47083600307500d4f254c09d2b673a0394a4b3e
Codfisher <pony2140@livemail.tw>
2024-05-17 17:38:00 +0800

    feat: 新增 fish

--- src/components/transition-shape/examples/basic-usage.vue
+++ src/components/transition-shape/examples/basic-usage.vue
@@ -1,27 +1,49 @@
 <template>
   <div class="flex flex-col items-start gap-4 w-full border border-gray-300 p-6">
     <base-checkbox
-      v-model="visible"
-      label="顯示"
+      v-model="state"
+      label="切換"
       class=" border p-4 rounded w-full"
     />
 
     <transition-shape appear>
       <div
-        v-if="visible"
+        v-if="state"
         class="text-xl"
       >
         我是一隻鱈魚 🐟
+      </div>
+    </transition-shape>
+
+    <transition-shape appear>
+      <div
+        :key="fish"
+        class="text-3xl"
+      >
+        {{ fish }}
       </div>
     </transition-shape>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 import BaseCheckbox from '../../base-checkbox.vue';
 import TransitionShape from '../transition-shape.vue';
+import { pipe, sample } from 'remeda';
 
-const visible = ref(true);
+const state = ref(true);
+
+const fish = ref('🐟');
+const fishList = [
+  '🐟', '🐋', '🐠', '🐡'
+];
+watch(state, () => {
+  fish.value = pipe(
+    fishList,
+    sample(1),
+    ([value]) => value ?? fishList[0]!
+  );
+});
 </script>
