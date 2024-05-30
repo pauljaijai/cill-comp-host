76e2670b6ba56700e576f7dccf45495acc92af2e
Codfisher <pony2140@livemail.tw>
2024-05-31 00:22:23 +0800

    feat: 新增背景色

--- src/components/bg-firefly/examples/basic-usage.vue
+++ src/components/bg-firefly/examples/basic-usage.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="flex flex-col gap-4 w-full border border-gray-300">
-    <bg-firefly class="bg-black w-full h-full" />
+    <bg-firefly class="bg w-full h-full" />
   </div>
 </template>
 
 <script setup lang="ts">
 import BgFirefly from '../bg-firefly.vue';
 </script>
+
+<style scoped lang="sass">
+.bg
+  background: linear-gradient(to top, #102b19, #191f1b)
+</style>
