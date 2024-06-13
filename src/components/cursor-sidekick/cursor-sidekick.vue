a79e12475808d18263605bb935f2e9a2f026e38d
Codfisher <pony2140@livemail.tw>
2024-06-13 23:52:30 +0800

    feat: 完成跟隨邏輯

--- src/components/cursor-sidekick/cursor-sidekick.vue
+++ src/components/cursor-sidekick/cursor-sidekick.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class=" fixed left-0 top-0">
+  <div class="sidekick fixed left-0 top-0 ">
     <svg
       :width="props.size"
       :height="props.size"
       viewBox="0 0 465 465"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      :style="style"
+      class=" "
     >
       <circle
         cx="232.5"
@@ -32,12 +34,15 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { CSSProperties, computed, ref } from 'vue';
+import { throttleFilter, useMouse, useRafFn } from '@vueuse/core';
 
 // #region Props
 interface Props {
   size?: string;
-  /** # 前綴之 HEX 格式 */
+  /** \# 前綴之 HEX 格式
+   * @default '#515151'
+   */
   color?: string;
 }
 // #endregion Props
@@ -52,10 +57,39 @@ const emit = defineEmits<{
 }>();
 // #endregion Emits
 
+const mouseInfo = useMouse({
+  eventFilter: throttleFilter(15),
+})
+
+const position = ref({
+  x: 0,
+  y: 0,
+});
+
+const style = computed<CSSProperties>(() => {
+  return {
+    transform: `translate(${position.value.x}px, ${position.value.y}px)`,
+  }
+});
+
+useRafFn(() => {
+  const delta = {
+    x: mouseInfo.x.value - position.value.x,
+    y: mouseInfo.y.value - position.value.y,
+  }
+
+  position.value.x += delta.x / 5;
+  position.value.y += delta.y / 5;
+})
+
+
+
 // #region Methods
 defineExpose({});
 // #endregion Methods
 </script>
 
 <style scoped lang="sass">
+.sidekick
+  transform: translate(-50%, -50%)
 </style>
