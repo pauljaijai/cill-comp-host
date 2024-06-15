d38b4dc529daff8caaab29ee87c665a8168d2447
Codfisher <pony2140@livemail.tw>
2024-06-15 08:38:31 +0800

    feat: 加入範例內容

--- src/components/cursor-sidekick/examples/basic-usage.vue
+++ src/components/cursor-sidekick/examples/basic-usage.vue
@@ -1,6 +1,41 @@
 <template>
   <div class="flex flex-col gap-4 w-full border border-gray-300 p-6">
-    <cursor-sidekick class="z-[100]" />
+    <cursor-sidekick />
+
+    <div class="flex flex-col gap-2">
+      <div class=" text-2xl font-bold">
+        我是標題
+      </div>
+
+      <div class="">
+        我是一段文字
+      </div>
+
+      <hr>
+
+      <base-input label="文字輸入框" />
+
+      <hr>
+
+      <base-btn label="按鈕" />
+      <base-btn
+        disabled
+        label="不可以瑟瑟 (。・ω・。)"
+      />
+
+      <hr>
+
+      <base-checkbox label="檢核方塊" />
+
+      <hr>
+
+      <a
+        href="https://vocus.cc/salon/cod-aquarium"
+        target="_blank"
+      >
+        鱈魚的魚缸
+      </a>
+    </div>
   </div>
 </template>
 
@@ -9,5 +44,6 @@ import { ref } from 'vue';
 
 import BaseCheckbox from '../../base-checkbox.vue';
 import BaseInput from '../../base-input.vue';
+import BaseBtn from '../../base-btn.vue';
 import CursorSidekick from '../cursor-sidekick.vue';
 </script>
