f7bd40587ecc96652474acb85b72999affb4831e
Codfisher <pony2140@livemail.tw>
2024-07-29 00:51:36 +0800

    feat: 調整預設動畫

--- src/components/text-characters-transition/examples/basic-usage.vue
+++ src/components/text-characters-transition/examples/basic-usage.vue
@@ -6,11 +6,24 @@
       class="border p-4 rounded"
     />
 
-    <div class="flex">
+    <div class="flex flex-col gap-2">
       <text-characters-transition
         :visible="visible"
         label="我是鱈魚🐟"
-        class="text-2xl"
+        class="text-2xl tracking-wider"
+      />
+
+      <text-characters-transition
+        :visible="visible"
+        label="一段很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長很長的文字"
+        :default-enter="() => ({
+          opacity: [0, 1],
+          delay: (el, i) => i * 5,
+        })"
+        :default-leave="() => ({
+          opacity: [1, 0],
+          delay: (el, i) => i * 5,
+        })"
       />
     </div>
   </div>
--- src/components/text-characters-transition/text-characters-transition.vue
+++ src/components/text-characters-transition/text-characters-transition.vue
@@ -7,7 +7,7 @@
       v-for="char, i in chars"
       :key="i"
       aria-hidden
-      :class="targetClass"
+      :class="char.class"
       class="inline-block"
     >
       {{ char.value }}
@@ -18,13 +18,23 @@
 <script setup lang="ts">
 import anime from 'animejs';
 import { nanoid } from 'nanoid';
-import { isNullish, map, pipe } from 'remeda';
-import { computed, onMounted, ref, watch } from 'vue';
+import { identity, isNullish, join, map, pipe, range, times } from 'remeda';
+import { computed, onMounted, ref, watch, CSSProperties } from 'vue';
+
+type AnimeFuncParam = (
+  index: number,
+  /** 動畫總數 */
+  length: number,
+) => Omit<anime.AnimeParams, 'targets'>;
 
 // #region Props
 interface Props {
   visible?: boolean;
-  label: string;
+  label: string | string[] | Array<{
+    value: string;
+    enter: AnimeFuncParam;
+    leave: AnimeFuncParam;
+  }>;
   /** html tag
    * 
    * @default 'p'
@@ -32,9 +42,14 @@ interface Props {
   tag?: string;
   /** 如何切割文字
    * 
+   * 只有在 label 為 string 時有效
+   * 
    * @default /.*?/u
    */
   splitter?: RegExp | ((label: string) => string[]);
+
+  defaultEnter?: AnimeFuncParam;
+  defaultLeave?: AnimeFuncParam;
 }
 // #endregion Props
 const props = withDefaults(defineProps<Props>(), {
@@ -42,6 +57,16 @@ const props = withDefaults(defineProps<Props>(), {
   label: '',
   tag: 'p',
   splitter: undefined,
+
+  defaultEnter: () => ({
+    opacity: [0, 1],
+    delay: (el, i) => i * 50,
+  } satisfies anime.AnimeParams),
+
+  defaultLeave: () => ({
+    opacity: [1, 0],
+    delay: (el, i) => i * 50,
+  } satisfies anime.AnimeParams),
 });
 
 // #region Emits
@@ -51,64 +76,109 @@ const emit = defineEmits<{
 // #endregion Emits
 
 const targetClass = nanoid();
-const labelText = computed(() => props.label);
+const target = `.${targetClass}`;
 
 const chars = computed(() => pipe(
   props.label,
-  (label) => {
-    if (typeof props.splitter === 'function') {
-      return props.splitter(label);
+  (data) => {
+    if (Array.isArray(data)) {
+      return data;
     }
 
-    return label.split(props.splitter ?? /.*?/u);
+    if (typeof props.splitter === 'function') {
+      return props.splitter(data);
+    }
+
+    /** Regex 加上 u 才不會導致 emoji 被拆分成亂碼 */
+    return data.split(props.splitter ?? /.*?/u);
   },
-  map((char) => {
+  map.strict.indexed((data, i, array) => {
+    if (typeof data === 'string') {
+      return {
+        value: data,
+        class: targetClass,
+        i,
+        enter: () => props.defaultEnter(i, array.length),
+        leave: () => props.defaultLeave(i, array.length),
+      }
+    }
+
     return {
-      value: char,
+      ...data,
+      i,
     }
   })
 ));
 
+const labelText = computed(() => pipe(
+  chars.value,
+  map((char) => char.value),
+  join(''),
+));
+
 async function enter() {
-  anime({
-    targets: `.${targetClass}`,
-    opacity: [0, 1],
-    translateX: () => [
-      anime.random(-30, -10),
-      0,
-    ],
-    translateY: () => [
-      anime.random(-20, 20),
-      0,
-    ],
-    filter: ['blur(10px)', 'blur(0px)'],
-    delay(el, i) {
-      return i * 100;
-    },
-    easing: 'easeOutCirc',
+  chars.value.forEach((char) => {
+    anime({
+      ...char.enter(char.i, chars.value.length),
+      targets: target
+    });
   });
+
+  // anime({
+  //   targets: target,
+  //   opacity: [0, 1],
+  //   translateX: () => [
+  //     anime.random(-30, -10),
+  //     0,
+  //   ],
+  //   translateY: () => [
+  //     anime.random(-20, 20),
+  //     0,
+  //   ],
+  //   rotate: () => [
+  //     anime.random(-30, 30),
+  //     0,
+  //   ],
+  //   filter: ['blur(10px)', 'blur(0px)'],
+  //   delay: (el, i) => i * 100,
+  //   easing: 'easeOutCirc',
+  // });
 }
 
 async function leave(duration?: number) {
   if (!isNullish(duration)) {
-    anime({
-      targets: `.${targetClass}`,
-      opacity: [1, 0],
-      duration,
-      delay(el, i) {
-        return i * 100;
-      }
+    chars.value.forEach((char) => {
+      anime({
+        ...char.leave(char.i, chars.value.length),
+        duration,
+        targets: target,
+      });
     });
     return;
   }
 
-  anime({
-    targets: `.${targetClass}`,
-    opacity: [1, 0],
-    delay(el, i) {
-      return i * 100;
-    }
+  chars.value.forEach((char) => {
+    anime({
+      ...char.leave(char.i, chars.value.length),
+      targets: target,
+    });
   });
+
+  // if (!isNullish(duration)) {
+  //   anime({
+  //     targets: target,
+  //     opacity: [1, 0],
+  //     duration,
+  //     delay: (el, i) => i * 50,
+  //   });
+  //   return;
+  // }
+
+  // anime({
+  //   targets: target,
+  //   opacity: [1, 0],
+  //   delay: (el, i) => i * 50,
+  // });
 }
 
 watch(() => props.visible, (visible) => {
