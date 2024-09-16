7224d88261e2ef411599d242f6b007e64456aaef
Codfisher <pony2140@livemail.tw>
2024-09-16 20:52:08 +0800

    refactor: 簡化型別定義

--- src/components/wrapper-physics/index.ts
+++ src/components/wrapper-physics/index.ts
@@ -1,23 +1,8 @@
-import { ComputedRef, InjectionKey } from "vue";
+import { InjectionKey } from 'vue';
 import Matter from 'matter-js';
 
-
-export const PROVIDE_KEY = Symbol('wrapper-physics') as InjectionKey<ProvideContent>;
-
-export interface Body {
-  id: string;
-  width: number;
-  height: number;
-  x: number;
-  y: number;
-
-  /** 物體形狀，預設為 rectangle
-   * 
-   * - rectangle：尺寸同 DOM 之長寬
-   * - circle：取 DOM 長寬最大值為直徑
-   */
-  polygon?: 'rectangle' | 'circle';
-
+/** DOM 物體基本參數 */
+interface BasicBodyParam {
   /** 空氣阻力。物體在空氣中受到的阻力 */
   frictionAir?: number;
   /** 摩擦力。物體本身的摩擦力，必須為 0 ~ 1，0 表示持續滑動，1 表示受力後會立即停止 */
@@ -30,17 +15,22 @@ export interface Body {
   isStatic?: boolean;
 }
 
-export interface UpdateParam {
-  /** 空氣阻力。物體在空氣中受到的阻力 */
-  frictionAir?: number;
-  /** 摩擦力。物體本身的摩擦力，必須為 0 ~ 1，0 表示持續滑動，1 表示受力後會立即停止 */
-  friction?: number;
-  /** 回彈力。碰撞的回彈係數，0 表示不反彈，1 表示完全反彈 */
-  restitution?: number;
-  /** 物體質量 */
-  mass?: number;
-  /** 靜止。會變成像地面那樣完全靜止的存在 */
-  isStatic?: boolean;
+export interface Body extends BasicBodyParam {
+  id: string;
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+
+  /** 物體形狀，預設為 rectangle
+   * 
+   * - rectangle：尺寸同 DOM 之長寬
+   * - circle：取 DOM 長寬最大值為直徑
+   */
+  polygon?: 'rectangle' | 'circle';
+}
+
+export interface UpdateParam extends BasicBodyParam {
   /** 速度，影響移動 */
   velocity?: Matter.Vector;
   /** 設定角速度，影響旋轉 */
@@ -58,3 +48,5 @@ export interface ProvideContent {
     rotate: number;
   } | undefined;
 }
+
+export const PROVIDE_KEY = Symbol('wrapper-physics') as InjectionKey<ProvideContent>;
