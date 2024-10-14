0e2a043c9ba862821ceb996657ee7bf262b8816d
Codfisher <pony2140@livemail.tw>
2024-10-15 01:09:56 +0800

    feat: 文字立體效果實驗成功

--- src/components/carousel-anamorphosis-text/carousel-anamorphosis-text.vue
+++ src/components/carousel-anamorphosis-text/carousel-anamorphosis-text.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="carousel-anamorphosis-text relative overflow-hidden">
+    <div class="bg">
+      文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字
+      文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字
+    </div>
+    <img src="/painting-codfish-bakery.webp">
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -27,4 +34,24 @@ defineExpose({})
 </script>
 
 <style scoped lang="sass">
+.carousel-anamorphosis-text
+  perspective: 1000px
+.bg
+  position: absolute
+  inset: 0
+  font-size: 10rem
+  background: url('/painting-codfish-bakery.webp')
+  background-size: cover
+  background-clip: text
+  overflow: hidden
+  color: #de466c
+  -webkit-text-fill-color: transparent
+  transform: translateZ(100px)
+  animation: in 1s forwards
+
+@keyframes in
+  0%
+    transform: translateZ(100px) rotateY(0deg)
+  100%
+    transform: translateZ(0) rotateY(0)
 </style>
--- src/components/carousel-anamorphosis-text/examples/basic-usage.vue
+++ src/components/carousel-anamorphosis-text/examples/basic-usage.vue
@@ -1,12 +1,5 @@
 <template>
-  <div class="w-full flex flex-col gap-4 border border-gray-300 p-6">
-    <div class="flex flex-col gap-4 border rounded p-4">
-      <base-checkbox
-        v-model="disabled"
-        label="停用按鈕"
-      />
-    </div>
-
+  <div class="w-full flex flex-col gap-4 border border-gray-300">
     <div class="flex justify-center">
       <carousel-anamorphosis-text />
     </div>
@@ -14,10 +7,5 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import BaseCheckbox from '../../base-checkbox.vue'
 import CarouselAnamorphosisText from '../carousel-anamorphosis-text.vue'
-
-const text = ref('')
-const disabled = ref(true)
 </script>
