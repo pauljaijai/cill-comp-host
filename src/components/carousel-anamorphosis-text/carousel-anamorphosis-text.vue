02f003de471d57d0ab17bdd917e9c87eb470de22
Codfisher <pony2140@livemail.tw>
2024-10-15 01:19:54 +0800

    feat: 實驗多層文字效果

--- src/components/carousel-anamorphosis-text/carousel-anamorphosis-text.vue
+++ src/components/carousel-anamorphosis-text/carousel-anamorphosis-text.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="carousel-anamorphosis-text relative overflow-hidden">
-    <div class="bg">
-      文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字
-      文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字
+    <div class="text-layer">
+      codfish codfish codfish codfish codfish codfish codfish codfish codfish codfish codfish
+      codfish codfish codfish codfish codfish codfish codfish codfish codfish codfish codfish
     </div>
+
+    <div class="text-layer">
+      fish fish fish fish fish fish fish fish fish fish fish
+      fish fish fish fish fish fish fish fish fish fish fish
+    </div>
+
+    <div class="text-layer">
+      cod cod cod cod cod cod cod cod cod cod cod
+      cod cod cod cod cod cod cod cod cod cod cod
+    </div>
+
     <img src="/painting-codfish-bakery.webp">
   </div>
 </template>
@@ -36,10 +47,16 @@ defineExpose({})
 <style scoped lang="sass">
 .carousel-anamorphosis-text
   perspective: 1000px
-.bg
+
+.text-layer
   position: absolute
   inset: 0
   font-size: 10rem
+  line-height: 1
+  font-weight: bold
+  word-break: break-all
+  white-space: pre-wrap
+  word-wrap: break-word
   background: url('/painting-codfish-bakery.webp')
   background-size: cover
   background-clip: text
@@ -47,11 +64,36 @@ defineExpose({})
   color: #de466c
   -webkit-text-fill-color: transparent
   transform: translateZ(100px)
-  animation: in 1s forwards
+  animation: in-1 3s forwards
+  opacity: 0
+  &:nth-child(2)
+    animation: in-2 3s forwards
+    animation-delay: 600ms
+  &:nth-child(3)
+    animation: in-3 3s forwards
+    animation-delay: 1200ms
 
-@keyframes in
+@keyframes in-1
   0%
-    transform: translateZ(100px) rotateY(0deg)
+    transform: translateZ(1000px) rotateZ(30deg)
+    opacity: 1
   100%
     transform: translateZ(0) rotateY(0)
+    opacity: 1
+
+@keyframes in-2
+  0%
+    transform: translateZ(1000px) rotateZ(30deg)
+    opacity: 1
+  100%
+    transform: translateZ(0) rotateY(0)
+    opacity: 1
+
+@keyframes in-3
+  0%
+    transform: translateZ(1000px) rotateZ(10deg)
+    opacity: 1
+  100%
+    transform: translateZ(0) rotateY(0)
+    opacity: 1
 </style>
