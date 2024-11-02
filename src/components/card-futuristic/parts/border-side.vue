0e21538b1098f311624cc7a4ac08d4e0a7b53b8a
Codfisher <pony2140@livemail.tw>
2024-11-03 00:34:59 +0800

    feat: 新增 border-side

---
+++ src/components/card-futuristic/parts/border-side.vue
@@ -0,0 +1,298 @@
+<template>
+  <svg
+    ref="svgRef"
+    :view-box
+    :style
+    fill="none"
+    class="card-border"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g>
+      <line
+        v-bind="lineStyleMap.t"
+        y1="0"
+        y2="0"
+        :stroke="prop.color"
+        stroke-width="1"
+      />
+      <line
+        v-bind="lineStyleMap.l"
+        x1="0"
+        x2="0"
+        :stroke="prop.color"
+        stroke-width="1"
+      />
+      <line
+        v-bind="lineStyleMap.b"
+        :y1="cardSize.height"
+        :y2="cardSize.height"
+        :stroke="prop.color"
+        stroke-width="1"
+      />
+      <line
+        v-bind="lineStyleMap.r"
+        :x1="cardSize.width"
+        :x2="cardSize.width"
+        :stroke="prop.color"
+        stroke-width="1"
+      />
+    </g>
+  </svg>
+</template>
+
+<script setup lang="ts">
+import type { AnimeMap } from '../type'
+import { reactiveComputed } from '@vueuse/core'
+import anime from 'animejs'
+import { map, pipe } from 'remeda'
+import { computed, inject, onMounted, ref } from 'vue'
+import { PROVIDE_KEY } from '../type'
+
+// #region Props
+export interface Props {
+  color?: string;
+  selectedColor?: string;
+  side?: Record<
+    't' | 'l' | 'b' | 'r',
+    {
+      color?: string;
+      selectedColor?: string;
+      strokeWidth?: number;
+    }
+  >;
+}
+// #endregion Props
+const prop = withDefaults(defineProps<Props>(), {
+  color: '#777',
+  selectedColor: '#ff8d0a',
+})
+
+const svgRef = ref<SVGAElement>()
+
+const card = inject(PROVIDE_KEY)
+const cardSize = reactiveComputed(() => ({
+  width: card?.contentSize.value.width ?? 0,
+  height: card?.contentSize.value.height ?? 0,
+}))
+
+const style = computed(() => ({
+  width: `${cardSize.width}px`,
+  height: `${cardSize.height}px`,
+}))
+
+const viewBox = computed(
+  () => `0 0 ${cardSize.width} ${cardSize.height}`,
+)
+
+const lineStyleMap = ref({
+  t: { x1: 0, x2: 0 },
+  l: { y1: 0, y2: 0 },
+  b: { x1: 0, x2: 0 },
+  r: { y1: 0, y2: 0 },
+})
+
+function removeAnime() {
+  anime.remove(Object.values(lineStyleMap.value))
+}
+
+const animeMap: AnimeMap = {
+  async normal(param) {
+    removeAnime()
+
+    const {
+      duration = 400,
+      delay = 0,
+    } = param ?? {}
+
+    const tasks = [
+      ...pipe(
+        [
+          lineStyleMap.value.t,
+          lineStyleMap.value.b,
+        ],
+        map((targets) => anime({
+          targets,
+          x1: 0,
+          x2: cardSize.width,
+          duration,
+          delay,
+          easing: 'easeOutExpo',
+        }).finished),
+      ),
+      ...pipe(
+        [
+          lineStyleMap.value.l,
+          lineStyleMap.value.r,
+        ],
+        map((targets) => anime({
+          targets,
+          y1: 0,
+          y2: cardSize.height,
+          duration,
+          delay,
+          easing: 'easeOutExpo',
+        }).finished),
+      ),
+    ]
+
+    await Promise.all(tasks)
+  },
+  async visible(param) {
+    removeAnime()
+
+    const {
+      duration = 400,
+      delay = 0,
+    } = param ?? {}
+
+    const tasks = [
+      ...pipe(
+        [
+          lineStyleMap.value.t,
+          lineStyleMap.value.b,
+        ],
+        map((targets) => anime({
+          targets,
+          x1: 0,
+          x2: cardSize.width,
+          duration,
+          delay,
+          easing: 'easeOutExpo',
+        }).finished),
+      ),
+      ...pipe(
+        [
+          lineStyleMap.value.l,
+          lineStyleMap.value.r,
+        ],
+        map((targets) => anime({
+          targets,
+          y1: 0,
+          y2: cardSize.height,
+          duration,
+          delay: 200 + delay,
+          easing: 'easeOutExpo',
+        }).finished),
+      ),
+    ]
+
+    await Promise.all(tasks)
+  },
+  async hidden(param) {
+    removeAnime()
+
+    const {
+      duration = 400,
+      delay = 0,
+    } = param ?? {}
+
+    const tasks = [
+      ...pipe(
+        [
+          lineStyleMap.value.t,
+          lineStyleMap.value.b,
+        ],
+        map((targets) => anime({
+          targets,
+          x1: cardSize.width / 2,
+          x2: cardSize.width / 2,
+          duration,
+          delay,
+          easing: 'easeInOutCirc',
+        }).finished),
+      ),
+
+      ...pipe(
+        [
+          lineStyleMap.value.l,
+          lineStyleMap.value.r,
+        ],
+        map((targets) => anime({
+          targets,
+          y1: cardSize.height / 2,
+          y2: cardSize.height / 2,
+          duration,
+          delay: 200 + delay,
+          easing: 'easeInOutCirc',
+        }).finished),
+      ),
+    ]
+
+    await Promise.all(tasks)
+  },
+  async selected(param) {
+    this.hover(param)
+  },
+  async hover(param) {
+    removeAnime()
+
+    const {
+      duration = 400,
+      delay = 0,
+    } = param ?? {}
+
+    const offset = 10
+
+    const tasks = [
+      ...pipe(
+        [
+          lineStyleMap.value.t,
+          lineStyleMap.value.b,
+        ],
+        map((targets) => anime({
+          targets,
+          x1: offset,
+          x2: cardSize.width - offset,
+          duration,
+          delay,
+          easing: 'easeOutExpo',
+        }).finished),
+      ),
+      ...pipe(
+        [
+          lineStyleMap.value.l,
+          lineStyleMap.value.r,
+        ],
+        map((targets) => anime({
+          targets,
+          y1: offset,
+          y2: cardSize.height - offset,
+          duration,
+          delay,
+          easing: 'easeOutExpo',
+        }).finished),
+      ),
+    ]
+
+    await Promise.all(tasks)
+  },
+}
+
+onMounted(() => {
+  if (!card) {
+    console.warn('此元件必須包在 CardFuturistic 元件中')
+    return
+  }
+
+  card.bindPart({
+    name: 'border',
+    animeMap,
+  })
+})
+
+// watch(() => card?.visible.value, (visible) => {
+//   console.log(`🚀 ~ visible:`, visible)
+
+//   if (visible) {
+//     animeMap.visible()
+//   }
+//   else {
+//     animeMap.hidden()
+//   }
+// }, {
+//   deep: true,
+// })
+</script>
+
+<style scoped lang="sass">
+</style>
