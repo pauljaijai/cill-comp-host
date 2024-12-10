87d5fde59d288591856efe9bdabb744b6ca152e5
bgm.cod <bgm.codfish@gmail.com>
2024-12-10 17:37:49 +0800

    feat: 新增 donate-section

---
+++ src/components/donate-section.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="vp-doc mb-10 border rounded-xl p-4">
+    如果你喜歡酷酷的元件，
+    <a
+      href="https://portaly.cc/codfish/support"
+      target="_blank"
+    >
+      歡迎贊助我一杯咖啡
+    </a>，鱈魚感謝您！(´▽`ʃ💖ƪ)
+  </div>
+</template>
+
+<style scoped lang="sass">
+</style>
