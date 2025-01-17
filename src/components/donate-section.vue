12ae2fec9219dcba1ea64786f48cc160e824a943
bgm.cod <bgm.codfish@gmail.com>
2025-01-17 13:30:56 +0800

    feat: 新增 LikeCoin

--- src/components/donate-section.vue
+++ src/components/donate-section.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="vp-doc mb-10 border rounded-xl p-4">
-    如果你喜歡酷酷的元件，
+    如果你喜歡酷酷的元件，歡迎「
     <a
       href="https://portaly.cc/codfish/support"
       target="_blank"
     >
-      歡迎贊助我一杯咖啡
-    </a>，鱈魚感謝您！(´▽`ʃ💖ƪ)
+      請我喝一杯咖啡
+    </a>
+
+    」或花 10 秒登入 LikeCoin 按下方按鈕拍手鼓勵我喔！
+    <iframe
+      :key="src"
+      scrolling="no"
+      frameborder="0"
+      :src
+    />
+
+    <div class="rounded-xl bg-slate-50 p-4 text-center text-lg text-gray-600">
+      鱈魚感謝您！ <span class="text-nowrap">(´▽`ʃ💖ƪ)</span>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const src = computed(() => {
+  const referrer = encodeURIComponent(window.location.href.replace('.html', ''))
+  return `https://button.like.co/in/embed/codlin/button?referrer=${referrer}`
+})
+</script>
 
 <style scoped lang="sass">
 </style>
