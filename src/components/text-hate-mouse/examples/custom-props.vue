ae49d9737034d4a329d2b24fd3adb080131adb62
Codfisher <pony2140@livemail.tw>
2025-01-29 18:24:03 +0800

    feat: 新增範例

--- src/components/text-hate-mouse/examples/custom-props.vue
+++ src/components/text-hate-mouse/examples/custom-props.vue
@@ -1,30 +1,50 @@
 <template>
   <div class="w-full flex flex-col gap-4 border border-gray-300 p-6 text-center">
-    <text-hate-mouse
-      v-for="item, i in list"
-      :key="i"
-      v-bind="item"
-    />
+    <div class="flex flex-col p-4">
+      <base-input
+        v-model="params.text"
+        label="自定義文字"
+        type="text"
+        class="w-full p-2 text-left"
+      />
+
+      <base-input
+        v-model.number="params.stiffness"
+        :label="`約束力：${params.stiffness}`"
+        type="range"
+        class="w-full p-2 text-left"
+        :max="0.3"
+        :step="0.001"
+        :min="0.001"
+      />
+
+      <base-input
+        v-model.number="params.damping"
+        :label="`阻尼：${params.damping}`"
+        type="range"
+        class="w-full p-2 text-left"
+        :max="1"
+        :step="0.01"
+        :min="0"
+      />
+    </div>
+
+    <text-hate-mouse v-bind="params" />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { ExtractComponentProps } from '../../../types'
-import { times } from 'remeda'
+import { ref } from 'vue'
+import BaseInput from '../../base-input.vue'
 import TextHateMouse from '../text-hate-mouse.vue'
 
 type Props = ExtractComponentProps<typeof TextHateMouse>
 
-const list: Props[] = [
-  {
-    text: '怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕',
-    stiffness: 0.2,
-    damping: 0,
-    evasionRadius: 30,
-  },
-  {
-    text: times(10, () => '逆走開'),
-    evasionRadius: 200,
-  },
-]
+const params = ref({
+  text: '字字字字字字',
+  evasionRadius: 40,
+  stiffness: 0.01,
+  damping: 0.05,
+} satisfies Props)
 </script>
---
+++ src/components/text-hate-mouse/examples/more-case.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w-full flex flex-col gap-4 border border-gray-300 p-6 text-center">
+    <text-hate-mouse
+      v-for="item, i in list"
+      :key="i"
+      v-bind="item"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ExtractComponentProps } from '../../../types'
+import { times } from 'remeda'
+import TextHateMouse from '../text-hate-mouse.vue'
+
+type Props = ExtractComponentProps<typeof TextHateMouse>
+
+const list: Props[] = [
+  {
+    text: '怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕怕',
+    stiffness: 0.2,
+    damping: 0,
+    evasionRadius: 30,
+  },
+  {
+    text: times(10, () => '逆走開'),
+    evasionRadius: 200,
+  },
+]
+</script>
