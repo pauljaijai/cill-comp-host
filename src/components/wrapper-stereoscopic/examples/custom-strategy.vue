da1425897bc6beac418402e1c25fe23eea2bfd76
Codfisher <pony2140@livemail.tw>
2025-01-30 14:33:55 +0800

    feat: 完成 custom-strategy 範例

--- src/components/wrapper-stereoscopic/examples/custom-strategy.vue
+++ src/components/wrapper-stereoscopic/examples/custom-strategy.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full flex-center border border-gray-300 p-6">
+  <div class="w-full flex flex-col items-center justify-center gap-8 border border-gray-300 p-6">
     <div class="content flex items-start">
       <wrapper-stereoscopic
         v-slot="wrapper"
@@ -11,30 +11,51 @@
         >
           <wrapper-stereoscopic-layer v-slot="layer01">
             <div
-              class="flex-center px-14 py-6 text-2xl tracking-widest"
+              class="flex-center px-14 py-8 text-2xl tracking-widest"
               :style="layer01.style"
             >
-              按鈕
+              (´● ω ●`)
             </div>
           </wrapper-stereoscopic-layer>
         </div>
       </wrapper-stereoscopic>
+    </div>
+
+    <div class="w-full flex justify-between">
+      <base-btn
+        ref="btnRef1"
+        label="看這"
+      />
+      <base-btn
+        ref="btnRef2"
+        label="看這"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { ExtractComponentProps } from '../../../types'
+import { useMousePressed } from '@vueuse/core'
+import { computed, ref } from 'vue'
 import { mapNumber } from '../../../common/utils'
+import BaseBtn from '../../base-btn.vue'
 import WrapperStereoscopicLayer from '../wrapper-stereoscopic-layer.vue'
 import WrapperStereoscopic from '../wrapper-stereoscopic.vue'
 
 type Props = ExtractComponentProps<typeof WrapperStereoscopic>
 
+const btnRef1 = ref()
+const { pressed: btn1Pressed } = useMousePressed({ target: btnRef1 })
+
+const btnRef2 = ref()
+const { pressed: btn2Pressed } = useMousePressed({ target: btnRef2 })
+const isPressed = computed(() => btn1Pressed.value || btn2Pressed.value)
+
 const params: Props = {
   strategy(params) {
     if (
-      params.isOutside
+      (params.isOutside && !isPressed.value)
       || !params.enable
       || !params.isVisible
       || params.isPressed) {
@@ -51,9 +72,9 @@ const params: Props = {
     } = params
 
     return {
-      x: mapNumber(y, -width, width, -20, 20),
-      y: mapNumber(x, -height, height, -20, 20),
-      zOffset: 200,
+      x: mapNumber(y, -height, height, -30, 30),
+      y: mapNumber(x, -width, width, -40, 40),
+      zOffset: 100,
     }
   },
 }
