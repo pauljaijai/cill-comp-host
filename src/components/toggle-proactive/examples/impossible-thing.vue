a18c9c9244432c0eaeedd3760f7fbfc3034f26a1
Codfisher <pony2140@livemail.tw>
2025-02-15 23:55:31 +0800

    feat: impossible-thing 範例加入顏色變化

--- src/components/toggle-proactive/examples/impossible-thing.vue
+++ src/components/toggle-proactive/examples/impossible-thing.vue
@@ -1,21 +1,22 @@
 <template>
-  <div class="w-full flex-center border border-gray-300 p-6">
+  <div class="w-full flex-center border border-gray-300 p-10">
     <div class="flex flex-col gap-4">
-      <div
+      <label
         v-for="state in stateList"
         :key="state.label"
-        class="flex items-center justify-end gap-4"
+        class="flex cursor-pointer items-center justify-end gap-5"
       >
-        <div class="text-xl">
+        <div class="text-2xl">
           {{ state.label }}
         </div>
 
         <toggle-proactive
           ref="toggleRefList"
           v-model="state.value"
-          size="3rem"
+          v-bind="state"
+          size="3.5rem"
         />
-      </div>
+      </label>
     </div>
   </div>
 </template>
@@ -28,16 +29,61 @@ import ToggleProactive from '../toggle-proactive.vue'
 interface State {
   value: boolean;
   label: string;
+  furColor: string;
+  padColor: string;
 }
 
 type Toggle = InstanceType<typeof ToggleProactive>
 
 const toggleRefList = ref<[Toggle, Toggle, Toggle]>()
 
+const colorList = [
+  {
+    furColor: '#7DDAEA',
+    padColor: '#000',
+  },
+  {
+    furColor: '#FAFAFA',
+    padColor: '#FFA5A5',
+  },
+  {
+    furColor: '#DFC57B',
+    padColor: '#000',
+  },
+  {
+    furColor: '#8D6F64',
+    padColor: '#FFA5A5',
+  },
+  {
+    furColor: '#444',
+    padColor: '#FFA5A5',
+  },
+  {
+    furColor: '#F3F2F2',
+    padColor: '#000',
+  },
+] as const
+let colorIndex = 0
+
 const stateList = ref<[State, State, State]>([
-  { value: false, label: '要快' },
-  { value: false, label: '要好' },
-  { value: false, label: '要便宜' },
+  {
+    label: '要快',
+    value: false,
+    furColor: '#DFC57B',
+    padColor: '#FFF',
+  },
+  {
+    label: '要好',
+    value: false,
+    furColor: '#DFC57B',
+    padColor: '#FFF',
+  },
+  {
+    label: '要便宜',
+    value: false,
+    furColor: '#DFC57B',
+    padColor: '#FFF',
+  },
 ])
 const booleanList = computed(
   () => stateList.value.map((state) => state.value),
@@ -49,9 +95,9 @@ watch(booleanList, (value, oldValue) => {
     return
   }
 
-  /** 排除最後一個切換的開關 */
   const targetIndex = pipe(
     oldValue,
+    /** 排除最後一個切換的開關 */
     reduce(
       (acc: number[], boolValue, i) => boolValue ? [...acc, i] : acc,
       [],
@@ -59,6 +105,15 @@ watch(booleanList, (value, oldValue) => {
     sample(1),
     ([i]) => i ?? 0,
   )
+
+  /** 依序變換顏色 */
+  const color = colorList[colorIndex] ?? colorList[0]
+  colorIndex = (colorIndex + 1) % colorList.length
+
+  if (stateList.value[targetIndex]) {
+    stateList.value[targetIndex].furColor = color.furColor
+    stateList.value[targetIndex].padColor = color.padColor
+  }
 
   toggleRefList.value?.[targetIndex]?.toggle()
 })
