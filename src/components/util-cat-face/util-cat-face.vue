b75ab2e1097752207a280cfc9e66baefab984c92
Codfisher <pony2140@livemail.tw>
2025-02-16 12:53:11 +0800

    feat: 新增臉部動作

--- src/components/util-cat-face/util-cat-face.vue
+++ src/components/util-cat-face/util-cat-face.vue
@@ -20,7 +20,8 @@
 
 <script setup lang="ts">
 import type { FacialExpression } from './type'
-import { ref } from 'vue'
+import anime from 'animejs'
+import { ref, watch } from 'vue'
 import FaceEye from './face-eye.vue'
 import FaceEyebrow from './face-eyebrow.vue'
 import FaceMouth from './face-mouth.vue'
@@ -33,19 +34,72 @@ interface Props {
   eyeOffsetRadius?: number;
 }
 // #endregion Props
+
+// #region Emits
+interface Emits {
+  change: [];
+}
+// #endregion Emits
+
 const props = withDefaults(defineProps<Props>(), {
   facialExpression: 'neutral',
   strokeColor: 'black',
   eyeOffsetRadius: 40,
 })
 
-// #region Emits
-const emit = defineEmits<{
-  change: [];
-}>()
-// #endregion Emits
+const emit = defineEmits<Emits>()
 
 const faceRef = ref<HTMLElement>()
+
+const animeProviderMap: Record<
+  FacialExpression,
+  () => Promise<void>
+> = {
+  neutral: () => anime({
+    targets: faceRef.value,
+    rotate: 0,
+    translateX: 0,
+    translateY: 0,
+    duration: 200,
+  }).finished,
+  excited: () => anime({
+    targets: faceRef.value,
+    keyframes: [
+      { rotate: 5 },
+      { rotate: -5 },
+    ],
+    duration: 1400,
+    loop: true,
+  }).finished,
+  happy: () => anime({
+    targets: faceRef.value,
+    keyframes: [
+      { translateY: 5 },
+      { translateY: -5 },
+    ],
+    easing: 'easeInOutQuint',
+    direction: 'alternate',
+    duration: 1000,
+    loop: true,
+  }).finished,
+  sad: () => Promise.resolve(),
+  angry: () => Promise.resolve(),
+  surprised: () => Promise.resolve(),
+}
+
+watch(
+  () => props.facialExpression,
+  async (facialExpression) => {
+    if (faceRef.value) {
+      anime.remove(faceRef.value)
+    }
+
+    await animeProviderMap.neutral()
+    await animeProviderMap[facialExpression]()
+    emit('change')
+  },
+  { immediate: true },
+)
 
 // #region Methods
 defineExpose({})
