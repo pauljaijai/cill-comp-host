3006a71d4ddbbe3af8e428613f8b35f958456a15
Codfisher <pony2140@livemail.tw>
2025-02-25 23:35:15 +0800

    feat: util-cat-face\examples\basic-usage 新增按鈕

--- src/components/util-cat-face/examples/basic-usage.vue
+++ src/components/util-cat-face/examples/basic-usage.vue
@@ -20,17 +20,40 @@
         </option>
       </select>
     </div>
+
+    <div class="flex gap-4">
+      <base-btn
+        label="上一個"
+        class="flex-1"
+        @click="prev()"
+      />
+      <base-btn
+        label="下一個"
+        class="flex-1"
+        @click="next()"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useData } from 'vitepress'
 import { computed, ref } from 'vue'
+import BaseBtn from '../../base-btn.vue'
 import { FacialExpression } from '../type'
 import UtilCatFace from '../util-cat-face.vue'
 
 const facialExpression = ref<`${FacialExpression}`>('neutral')
-const options = Object.values(FacialExpression)
+const options = Object.values(FacialExpression) as `${FacialExpression}`[]
+
+function next() {
+  const index = options.indexOf(facialExpression.value)
+  facialExpression.value = options[(index + 1) % options.length] ?? FacialExpression.NEUTRAL
+}
+function prev() {
+  const index = options.indexOf(facialExpression.value)
+  facialExpression.value = options[(index - 1 + options.length) % options.length] ?? FacialExpression.NEUTRAL
+}
 
 const { isDark } = useData()
 const strokeColor = computed(() => isDark.value ? '#ddd' : '#222')
